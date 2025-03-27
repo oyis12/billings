95522cfae659ad7390e2a001253a4153556257e6
Oyivo Samuel <116066740+oyis12@users.noreply.github.com>
2025-03-27 11:09:42 +0100

    testing cors again

--- index.js
+++ index.js
@@ -16,20 +16,18 @@ const PORT = process.env.PORT || 5000
 app.use(express.json())
 app.use(cookieParser())
 
-// const corOptions = {
-//     origin: process.env.CLIENT_URL || ['http://localhost:3000'],
-//     credential: true,
-//     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE']
-// }
+const corOptions = {
+    origin: process.env.CLIENT_URL || ['http://localhost:5173'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE']
+}
 
 // const corOptions = {
 //     origin: process.env.CLIENT_URL || ['http://localhost:3000'],
 //     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE']
 // }
 
-// app.use(cors(corOptions))
-
-app.use(cors())
+app.use(cors(corOptions))
 
 // Routes
 app.use('/api/v1', authRoutes)
